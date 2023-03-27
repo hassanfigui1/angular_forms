9ccfc29c7862397fc254e1b67535681d64d5d46d
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-27 08:28:09 +0000

    creating a User instance

--- angular_forms/src/app/app.component.ts
+++ angular_forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_forms';
+  userModel = new User('Hassan FIGUIGUI','hassanfig@gmail.com', 7843784434, '','morning',true)
 }
