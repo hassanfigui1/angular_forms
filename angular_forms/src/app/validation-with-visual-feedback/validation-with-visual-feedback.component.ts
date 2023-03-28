11aafa33ba1ee00a68f6485bd68dede4a01e398f
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-03-28 17:57:00 +0000

    showing up errors for unselected options [select tag]

--- angular_forms/src/app/validation-with-visual-feedback/validation-with-visual-feedback.component.ts
+++ angular_forms/src/app/validation-with-visual-feedback/validation-with-visual-feedback.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./validation-with-visual-feedback.component.css']
 })
 export class ValidationWithVisualFeedbackComponent {
+  private tech:any[] = ['Django','Python','Java','Php','Flask','Angular','Angular'];
 
+
+  getTech(){
+    return this.tech;
+  }
 }
